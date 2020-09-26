d21694fa66bc115afe25acc86e940866f3e9756a
AanyaPrasad600 <70582742+AanyaPrasad600@users.noreply.github.com>
2020-09-26 12:17:37 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,77 +1,35 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+var movingRect, fixedRect;
+var gameObject1, gameObject2;
 
-var engine, world;
-var box1, pig1;
-var backgroundImg,platform;
-var bird, slingShot;
-
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.velocityY = 3;
+  movingRect = createSprite(400, 500, 80, 30);
+  movingRect.shapeColor = "green";
+  movingRect.velocityY = -3;
+  gameObject1 = createSprite(100,100,50,50);
+  gameObject1.shapeColor = "blue";
+  gameObject2 = createSprite(200,100,50,50);
+  gameObject2.shapeColor = "blue";
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+function draw() {
+  background(0,0,0); 
 
+  gameObject2.x = World.mouseX;
+  gameObject2.y = World.mouseY;
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+  if (isTouching(gameObject1, gameObject2)) {
+    gameObject1.shapeColor = "green";
+    gameObject2.shapeColor = "green";
+  }
+  else {
+    gameObject1.shapeColor = "blue";
+    gameObject2.shapeColor = "blue";
+  }
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(100,100);
-
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:100});
-}
-
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();    
-}
-
-function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-}
-
-
-function mouseReleased(){
-    slingshot.fly();
+  bounceOff(movingRect, fixedRect);
+  drawSprites();
 }
